65899e91be1e4e0b27c13afb229090e005b247e1
David Wales <davidwales2@gmail.com>
2017-04-20 11:16:55 +1000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Amazon.Lambda.Core;
+
+namespace lambda {
+    class Hello {
+        static void Main(string[] args) {}
+        public OutputObject MyHandler(InputObject input, ILambdaContext context) {
+            context.Logger.LogLine("MyHandler called with input num " + input.InputNum);
+            return new OutputObject("Success");
+        }
+    }
+
+    public class InputObject {
+        public int InputNum = 0;
+    }
+
+    public class OutputObject {
+        public String Response = "";
+        public OutputObject(string response) {
+            Response = response;
+        }
+    }
+}
