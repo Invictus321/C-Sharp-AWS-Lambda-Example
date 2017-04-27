d47d25b86d1011cae17907346495fed0ee047882
Invictus321 <davidwales2@gmail.com>
2017-04-27 15:52:38 +1000

    Change input and output objects to be API gateway request and response

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 using Amazon.Lambda.Core;
 
 namespace lambda {
@@ -7,20 +8,57 @@ namespace lambda {
         static void Main(string[] args) {}
         
         [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
-        public OutputObject MyHandler(InputObject input, ILambdaContext context) {
-            context.Logger.LogLine("MyHandler called with input num " + input.InputNum);
-            return new OutputObject("Success");
+        public Response MyHandler(Request input, ILambdaContext context) {
+            context.Logger.LogLine("MyHandler called with input body " + input.Body);
+            return new Response("Success");
         }
     }
 
-    public class InputObject {
-        public int InputNum = 0;
+    public class Request {
+        public String Resource;
+        public String Path;
+        public String HttpMethod;
+        public Dictionary<String, String> Headers;
+        public Dictionary<String, String> QueryStringParameters;
+        public Dictionary<String, String> PathParameters;
+        public Dictionary<String, String> StageVariables;
+        public RequestContext RequestContext;
+        public String Body;
+        public Boolean IsBase64Encoded;
     }
 
-    public class OutputObject {
-        public String Response = "";
-        public OutputObject(string response) {
-            Response = response;
+    public class RequestContext {
+        public String AccountId;
+        public String ResourceId;
+        public String Stage;
+        public String RequestId;
+        public Identity Identity;
+        public String ResourcePath;
+        public String HttpMethod;
+        public String ApiId;
+    }
+
+    public class Identity {
+        public String CognitoIdentityPoolId;
+        public String AccountId;
+        public String CognitoIdentityId;
+        public String Caller;
+        public String ApiKey;
+        public String SourceIp;
+        public String CognitoAuthenticationType;
+        public String CognitoAuthenticationProvider;
+        public String UserArn;
+        public String UserAgent;
+        public String User;
+    }
+
+    public class Response {
+        public int StatusCode;
+        public Dictionary<String, String> Headers;
+        public String Body;
+        public Response(string response) {
+            StatusCode = 200;
+            Body = response;
         }
     }
 }
